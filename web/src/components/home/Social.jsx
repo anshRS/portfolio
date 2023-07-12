a424e5d26f19da04a0c9e22a2425cae993b9eed3
anshRS <anshraj0426@gmail.com>
2023-07-13 01:46:44 +0530

    Created responsive Home section

---
+++ web/src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://gmail.com/" className="home__social-icon" target='_blank'>
+                <i className="ri-mail-line"></i>
+            </a>
+            <a href="https://linkedin.com/" className="home__social-icon" target='_blank'>
+                <i className="ri-linkedin-box-fill"></i>
+            </a>
+            <a href="https://github.com/" className="home__social-icon" target='_blank'>
+                <i className="ri-github-fill"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
