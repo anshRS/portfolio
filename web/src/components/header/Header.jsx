f2f66f5166dbf256103c46b353014294198f3ced
anshRS <anshraj0426@gmail.com>
2023-07-16 23:45:24 +0530

    Added auto active NavLink on scroll feature

--- web/src/components/header/Header.jsx
+++ web/src/components/header/Header.jsx
@@ -4,6 +4,26 @@ import './header.css'
 const Header = () => {
 
     useEffect(() => {
+        // Scroll Sections Active Link 
+        const sections = document.querySelectorAll('section[id]')
+
+        function scrollActive() {
+            const scrollY = window.scrollY
+
+            sections.forEach(current => {
+                const sectionHeight = current.offsetHeight,
+                    sectionTop = current.offsetTop - 58,
+                    sectionId = current.getAttribute('id')
+
+                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                    document.querySelector('.nav__list a[href*=' + sectionId + ']').classList.add('active-link')
+                } else {
+                    document.querySelector('.nav__list a[href*=' + sectionId + ']').classList.remove('active-link')
+                }
+            })
+        }
+        window.addEventListener('scroll', scrollActive)
+
         // Active Link
         const navlink = document.querySelectorAll('.nav__link');
 
