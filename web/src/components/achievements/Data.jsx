c61968f8e4310eea4e8095845c9171b20a2123a3
anshRS <anshraj0426@gmail.com>
2023-11-15 22:31:25 +0530

    Updated achievements data with STGI hackathon

--- web/src/components/achievements/Data.jsx
+++ web/src/components/achievements/Data.jsx
@@ -1,24 +1,32 @@
 import acm from '../../assets/acm.png'
 import nta from '../../assets/nta.png'
 import vvm from '../../assets/vvm.png'
+import stgi from '../../assets/stgi.png'
 
 export const Data = [
     {
         id: 1,
+        image: stgi,
+        title: "Second position in STG CODE BLOODED 5.0",
+        description:
+            "Our dedicated team achieved the 2nd place in the rigorous 24-hour hackathon organized by STGI Technologies Consulting Pvt Ltd from October 15 to 17, 2023. We secured this victory, earning a cash prize of 30,000 INR.",
+    },
+    {
+        id: 2,
         image: acm,
         title: "Third position in Ideathon 2.0",
         description:
             "Showcased exceptional talent and expertise, earning a prestigious 3rd place in the highly anticipated IDEATHON 2.0, a captivating team competition arranged by PEC ACM-CSS on the momentous occasion of 1st November, 2022.",
     },
     {
-        id: 2,
+        id: 3,
         image: nta,
         title: "JEE Main & Advanced",
         description:
             "Through perseverance and relentless effort, secured 7259 rank (99.3 percentile) in JEE Mains among more than a million candidates & 9242 rank in JEE Advanced among a total of 150,000+ candidates",
     },
     {
-        id: 3,
+        id: 4,
         image: vvm,
         title: "Third rank in Chandigarh",
         description:
