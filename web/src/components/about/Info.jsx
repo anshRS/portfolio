50d449bdf6ca3602d8ce3bf88d07513c231a794c
anshRS <anshraj0426@gmail.com>
2024-03-30 21:04:21 +0530

    Updated About section

--- web/src/components/about/Info.jsx
+++ web/src/components/about/Info.jsx
@@ -6,7 +6,7 @@ const Info = () => {
             <div className="about__box">
                 <i className="ri-medal-line about__icon"></i>
                 <h3 className="about__title">Experience</h3>
-                <span className="about__subtitle">Fresher</span>
+                <span className="about__subtitle">Developer</span>
             </div>
 
             <div className="about__box">
@@ -18,7 +18,7 @@ const Info = () => {
             <div className="about__box">
                 <i className="ri-funds-line about__icon"></i>                
                 <h3 className="about__title">GitHub</h3>
-                <span className="about__subtitle">400+ Contributions</span>
+                <span className="about__subtitle">500+ Contributions</span>
             </div>
         </div>
     )
